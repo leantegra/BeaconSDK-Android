517db65f3ada8dfa8b26fa1a67b029e22c15acb4
Artem Drozd <artemd@leantegra.com>
2017-03-20 13:26:13 +0200

    Demo updates

--- Demos/MobileRTLSDemo/app/src/main/java/com/leantegra/mobilertls/RTLSApplication.java
+++ Demos/MobileRTLSDemo/app/src/main/java/com/leantegra/mobilertls/RTLSApplication.java
@@ -4,16 +4,12 @@ import android.app.Application;
 
 import com.leantegra.wibeat.sdk.LeantegraSDK;
 
-/**
- * Created by tania on 19.01.17.
- */
-
 public class RTLSApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-        LeantegraSDK.initialize(this, "mobilertls_mobile", "8eb70dae-8e8a-47b6-bde2-fe1a52b392be");
-        LeantegraSDK.setServerAddress("35.157.31.110");
+        LeantegraSDK.initialize(this, "your client id", "your client secret");
+        LeantegraSDK.setServerAddress("CVO portal address");
     }
 }
